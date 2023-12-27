1bd77d9fa94a960797e8c4b6bc70843d106aa10a
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-12-27 11:29:40 +0900

    1. ConvMVVM.Core 프로젝트 추가

---
+++ ConvMVVM.Core/ConvMVVM.Core/Class1.cs
@@ -0,0 +1,8 @@
+
+namespace ConvMVVM.Core
+{
+    public class Class1
+    {
+    }
+
+}
---
+++ ConvMVVM.Core/ConvMVVM.Core/DI/IContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvMVVM.Core.DI
+{
+    public interface IContainer
+    {
+        public void RegisterCache<TInterface, TImplementation>() where TImplementation : TInterface ;
+        public void RegisterNonCache<TInterface, TImplementation>() where TImplementation: TInterface;
+        //public TInterface GetCache<TInterface>
+    }
+}
