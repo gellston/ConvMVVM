3cc24abd875f5162166f4b1feef7f0f007bd052b
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-12-31 16:35:27 +0900

    1. IocContainer 예제 업데이트

---
+++ ConvMVVM.Core/IOCContainerExample/Model/BModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCContainerExample.Model
+{
+    public class BModel
+    {
+        #region Constructor
+        public BModel() { }
+        #endregion
+    }
+}
