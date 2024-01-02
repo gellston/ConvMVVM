cd0a4cb95439b078c666ffcfc442b0c5c72d1b42
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2024-01-03 06:03:22 +0900

    1. RegionManager 예제 업데이트

---
+++ Examples/RegionManagerExample/RegionManagerExample/App.cs
@@ -0,0 +1,40 @@
+﻿using ConvMVVM.Core.IOC;
+using ConvMVVM.Core.Service.RegionManager;
+using ConvMVVM.WPF.Component;
+using ConvMVVM.WPF.Service;
+using RegionManagerExample.View;
+using RegionManagerExample.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RegionManagerExample
+{
+    public class App : ConvMVVMApp
+    {
+        protected override void ConfigureServiceLocator()
+        {
+            //ServiceLocator.RegionManager.RegisterCacheView<AView>();
+        }
+
+        protected override void ConfigureServiceCollection(IServiceCollection serviceCollection)
+        {
+            serviceCollection.RegisterCache<MainWindowViewModel>();
+            serviceCollection.RegisterCache<AViewModel>();
+        }
+
+        protected override void ConfigureServiceProvider(IServiceContainer serviceProvider)
+        {
+            var regionManager = serviceProvider.GetService<IRegionManager>();
+            regionManager.RegisterCacheView<AView>();
+        }
+
+        protected override Window CreateWindow(IServiceContainer serviceProvider)
+        {
+            return new MainWindowView();
+        }
+    }
+}
---
+++ Examples/RegionManagerExample/RegionManagerExample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ConvMVVM.Core.Attributes;
+using ConvMVVM.Core.Component;
+using ConvMVVM.Core.Service.RegionManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionManagerExample.ViewModel
+{
+    partial class MainWindowViewModel : NotifyObject
+    {
+        #region Private Property
+        private readonly IRegionManager regionManager;
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel(IRegionManager regionManager) { 
+        
+            this.regionManager = regionManager;
+        }
+        #endregion
+
+        #region Command
+        [RelayCommand]
+        public void Test()
+        {
+            this.regionManager.NavigateCache("MainContent", "AView");
+        }
+        #endregion
+    }
+}
