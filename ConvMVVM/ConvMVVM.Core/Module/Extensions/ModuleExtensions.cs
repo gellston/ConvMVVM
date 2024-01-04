42f4d59e2f11beb892d53fccdd9e0ec79692c23b
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2024-01-04 19:25:06 +0900

    1. Module 추가를 위한 Extension method 추가

---
+++ ConvMVVM/ConvMVVM.Core/Module/Extensions/ModuleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvMVVM.Core.Module.Extensions
+{
+    public static class ModuleExtensions
+    {
+        #region Static Functions
+        public static ModuleCollection AddModule<TModule>(this ModuleCollection moduleCollection) where TModule : class
+        {
+            IModule module = (IModule)Activator.CreateInstance(typeof(TModule));
+            moduleCollection.Add(module);
+            return moduleCollection;
+        }
+
+        #endregion
+
+
+    }
+}
