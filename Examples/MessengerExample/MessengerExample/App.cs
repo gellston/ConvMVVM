4e1ca21e5c8ccb28ec8b51aab176fdb25e3ba442
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2024-01-05 16:48:05 +0900

    1. Example ConvMVVM 버전 업데이트

---
+++ Examples/MessengerExample/MessengerExample/App.cs
@@ -0,0 +1,57 @@
+﻿using ConvMVVM.Core.IOC;
+using ConvMVVM.Core.Module;
+using ConvMVVM.Core.Service.RegionManager;
+using ConvMVVM.WPF.Component;
+using ConvMVVM.WPF.Service;
+using MessengerExample.View;
+using MessengerExample.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MessengerExample
+{
+    public class App : ConvMVVMApp
+    {
+
+        protected override void ConfigureServiceLocator()
+        {
+            ServiceLocator.RegionManager.RegisterCacheView<MainWindowView>();
+            ServiceLocator.RegionManager.RegisterCacheView<AView>();
+            ServiceLocator.RegionManager.RegisterCacheView<BView>();
+        }
+
+
+        protected override void ConfigureModule(ModuleCollection modules)
+        {
+
+        }
+
+
+        protected override void ConfigureServiceCollection(IServiceCollection serviceCollection)
+        {
+            serviceCollection.RegisterCache<MainWindowViewModel>();
+            serviceCollection.RegisterCache<AViewModel>();
+            serviceCollection.RegisterCache<BViewModel>();
+        }
+
+
+        protected override void ConfigureServiceProvider(IServiceContainer serviceProvider)
+        {
+    
+
+        }
+
+        protected override Window CreateWindow(IServiceContainer serviceProvider)
+        {
+            var regionManager = serviceProvider.GetService<IRegionManager>();
+            regionManager.NavigateCache("AView", "AView");
+            regionManager.NavigateCache("BView", "BView");
+
+            return new MainWindowView();
+        }
+    }
+}
